0994f94a98d708320b5a6265b9e372bf9bdb69b1
grbalmeida <g.r.almeida@live.com>
2023-11-06 16:35:04 -0300

    Dados estáticos

--- src/components/Tabela/index.tsx
+++ src/components/Tabela/index.tsx
@@ -8,13 +8,22 @@ function Tabela() {
                     <TableHead>
                         <TableRow>
                             <TableCell>Data</TableCell>
+                            <TableCell>Horário</TableCell>
+                            <TableCell>Profissional</TableCell>
+                            <TableCell>Especialidade</TableCell>
+                            <TableCell>Paciente</TableCell>
+                            <TableCell>Modalidade</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         <TableRow>
-                            <TableCell component="th" scope="row">22/03/2022</TableCell>
+                           <TableCell component="th" scope="row">22/02/2023</TableCell>
                             <TableCell>08:30</TableCell>
-                        </TableRow>
+                            <TableCell>Monica Hillman</TableCell>
+                            <TableCell>Clinica Geral</TableCell>
+                            <TableCell>Caroline Carvalho</TableCell>
+                            <TableCell>Particular</TableCell>
+                          </TableRow>
                     </TableBody>
                 </Table>
             </TableContainer>
