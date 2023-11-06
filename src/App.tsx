84ee14f81719cf6f8c21fa7ce25b815c8a50a965
grbalmeida <g.r.almeida@live.com>
2023-11-06 15:27:32 -0300

    Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css';
 import Cabecalho from './components/Cabecalho';
+import Container from './components/Container';
 
 function App() {
   return (
-    <Cabecalho />
+    <>
+      <Cabecalho />
+      <Container>
+        
+      </Container>
+    </>
   );
 }
 
