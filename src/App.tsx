d16af28ad22bdb36adfbb112630f43580d608c03
grbalmeida <g.r.almeida@live.com>
2023-11-06 15:38:42 -0300

    Título

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
 import Cabecalho from './components/Cabecalho';
 import Container from './components/Container';
+import Titulo from './components/Titulo';
 
 function App() {
   return (
     <>
       <Cabecalho />
       <Container>
-        
+        <Titulo>Área Administrativa</Titulo>
       </Container>
     </>
   );
