4f57dfcb6ae4ae1cc49fc784929e3159d478fe3a
grbalmeida <g.r.almeida@live.com>
2023-11-07 11:54:46 -0300

    Alterando url de api mockada para realizar deploy na Vercel

--- src/useFetch.ts
+++ src/useFetch.ts
@@ -4,8 +4,10 @@ export default function useFetch<T>({ url }: { url: string }) {
     const [dados, setDados] = useState<T | null>(null);
     const [erro, setErro] = useState('');
 
+    let apiUrl = process.env.API_URL ? process.env.API_URL : 'http://localhost:8080/';
+
     useEffect(() => {
-        fetch(`http://localhost:8080/${url}`)
+        fetch(`${apiUrl}${url}`)
             .then(resposta => resposta.json())
             .then(dados => setDados(dados))
             .catch(erro => setErro(erro));
